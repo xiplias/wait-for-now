fb3842d4c5fd2bb6ab26bc00f1c71b5331135586
Anders Hansen <xiplias@gmail.com>
2019-08-19 09:33:27 +0200

    Use ci-env for multi ci support

--- bin/src/index.ts
+++ bin/src/index.ts
@@ -1,24 +1,26 @@
 import Octokit from '@octokit/rest'
 import got from 'got'
 import pWaitFor from 'p-wait-for'
+import { repo as rep, sha as ref } from 'ci-env'
 
 const octokit = new Octokit()
 
-const [owner, repo] = process.env.TRAVIS_REPO_SLUG.split('/')
-const ref = process.env.TRAVIS_PULL_REQUEST_SHA
+const [owner, repo] = rep.split('/')
 
 const getSuccessfulDeployment = async () => {
   octokit.authenticate({ token: process.env.GITHUB_API_TOKEN, type: 'oauth' })
 
   const { data } = await octokit.repos.getStatuses({ owner, ref, repo })
-  const deployments = data.filter((item) => /^(deployment\/)?now$/.test(item.context))
+  const deployments = data.filter((item) =>
+    /^(deployment\/)?now$/.test(item.context)
+  )
 
   return deployments.find((item) => item.state === 'success')
 }
 
 const deployed = async () => (await getSuccessfulDeployment()) !== undefined
 
-// Run the checker
+  // Run the checker
 ;(async () => {
   await pWaitFor(deployed, { interval: 15000 })
 
